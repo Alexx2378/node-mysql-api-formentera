0f0955cd905c1f60bc902169c9f7e327ad8c4b7f
Alexx2378 <gaviolakarl.martin@gmail.com>
2026-05-22 11:29:33 +0800

    Fix swagger.yaml path for compiled output directory

--- src/_helpers/swagger.ts
+++ src/_helpers/swagger.ts
@@ -3,7 +3,7 @@ import YAML from 'yamljs';
 import path from 'path';
 
 export default function setupSwagger(app: any) {
-    const swaggerDocument = YAML.load(path.join(__dirname, '../../swagger.yaml'));
+    const swaggerDocument = YAML.load(path.join(__dirname, '../../../swagger.yaml'));
     app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
     console.log('📚 Swagger docs loaded');
 }
